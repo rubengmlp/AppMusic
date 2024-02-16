4e1e867ed2ba2fb334ba121ea953a7b73a6829da
ruben <ruben@DESKTOP-0ENH35G>
2024-02-16 16:47:12 +0100

    Vista de login creada

---
+++ AppMusic/src/main/java/umu/tds/vistas/VistaLogin.java
@@ -0,0 +1,180 @@
+package umu.tds.vistas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.BoxLayout;
+import java.awt.CardLayout;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+import java.awt.Component;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.SystemColor;
+
+public class VistaLogin {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VistaLogin window = new VistaLogin();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VistaLogin() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelTitulo = new JPanel();
+		frame.getContentPane().add(panelTitulo, BorderLayout.NORTH);
+		
+		JLabel lblTitulo = new JLabel("AppMusic");
+		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 26));
+		panelTitulo.add(lblTitulo);
+		
+		JPanel panelLogin = new JPanel();
+		frame.getContentPane().add(panelLogin, BorderLayout.CENTER);
+		GridBagLayout gbl_panelLogin = new GridBagLayout();
+		gbl_panelLogin.columnWidths = new int[]{15, 30, 167, 15, 0};
+		gbl_panelLogin.rowHeights = new int[]{20, 43, 43, 60, 43, 43, 0};
+		gbl_panelLogin.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_panelLogin.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panelLogin.setLayout(gbl_panelLogin);
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		GridBagConstraints gbc_lblUsuario = new GridBagConstraints();
+		gbc_lblUsuario.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUsuario.anchor = GridBagConstraints.EAST;
+		gbc_lblUsuario.gridx = 1;
+		gbc_lblUsuario.gridy = 1;
+		panelLogin.add(lblUsuario, gbc_lblUsuario);
+		
+		textField = new JTextField();
+		textField.setBorder(new LineBorder(SystemColor.activeCaptionBorder, 2, true));
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.fill = GridBagConstraints.BOTH;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 1;
+		panelLogin.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblContraseña = new JLabel("Contraseña");
+		GridBagConstraints gbc_lblContraseña = new GridBagConstraints();
+		gbc_lblContraseña.insets = new Insets(0, 0, 5, 5);
+		gbc_lblContraseña.anchor = GridBagConstraints.EAST;
+		gbc_lblContraseña.gridx = 1;
+		gbc_lblContraseña.gridy = 2;
+		panelLogin.add(lblContraseña, gbc_lblContraseña);
+		
+		textField_1 = new JTextField();
+		textField_1.setBorder(new LineBorder(SystemColor.activeCaptionBorder, 2, true));
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.fill = GridBagConstraints.BOTH;
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.gridx = 2;
+		gbc_textField_1.gridy = 2;
+		panelLogin.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JPanel panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.insets = new Insets(0, 0, 5, 5);
+		gbc_panel.fill = GridBagConstraints.VERTICAL;
+		gbc_panel.gridx = 2;
+		gbc_panel.gridy = 4;
+		panelLogin.add(panel, gbc_panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{154, 154, 0};
+		gbl_panel.rowHeights = new int[]{38, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JButton botonLoginGitHub = new JButton("Login con GitHub");
+		botonLoginGitHub.setBorder(new LineBorder(SystemColor.activeCaptionBorder, 2, true));
+		botonLoginGitHub.setAlignmentX(Component.CENTER_ALIGNMENT);
+		botonLoginGitHub.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton botonLogin = new JButton("Login");
+		botonLogin.setBorder(new LineBorder(SystemColor.activeCaptionBorder, 2, true));
+		botonLogin.setAlignmentX(Component.CENTER_ALIGNMENT);
+		GridBagConstraints gbc_botonLogin = new GridBagConstraints();
+		gbc_botonLogin.fill = GridBagConstraints.BOTH;
+		gbc_botonLogin.insets = new Insets(0, 0, 0, 5);
+		gbc_botonLogin.gridx = 0;
+		gbc_botonLogin.gridy = 0;
+		panel.add(botonLogin, gbc_botonLogin);
+		GridBagConstraints gbc_botonLoginGitHub = new GridBagConstraints();
+		gbc_botonLoginGitHub.fill = GridBagConstraints.BOTH;
+		gbc_botonLoginGitHub.gridx = 1;
+		gbc_botonLoginGitHub.gridy = 0;
+		panel.add(botonLoginGitHub, gbc_botonLoginGitHub);
+		
+		JPanel panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.insets = new Insets(0, 0, 0, 5);
+		gbc_panel_1.fill = GridBagConstraints.VERTICAL;
+		gbc_panel_1.gridx = 2;
+		gbc_panel_1.gridy = 5;
+		panelLogin.add(panel_1, gbc_panel_1);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[]{308, 0};
+		gbl_panel_1.rowHeights = new int[]{38, 0};
+		gbl_panel_1.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel_1.setLayout(gbl_panel_1);
+		
+		JButton btnRegistro = new JButton("Registro");
+		btnRegistro.setBorder(new LineBorder(SystemColor.activeCaptionBorder, 2, true));
+		btnRegistro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnRegistro = new GridBagConstraints();
+		gbc_btnRegistro.fill = GridBagConstraints.VERTICAL;
+		gbc_btnRegistro.gridx = 0;
+		gbc_btnRegistro.gridy = 0;
+		panel_1.add(btnRegistro, gbc_btnRegistro);
+	}
+
+}
