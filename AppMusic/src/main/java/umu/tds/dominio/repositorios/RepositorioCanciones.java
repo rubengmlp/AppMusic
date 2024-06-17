44779a603e78b9e20f379b3788e029c007752f49
Ruben <rubengmlp@gmail.com>
2024-06-17 20:42:52 +0200

    Ajustes

--- AppMusic/src/main/java/umu/tds/dominio/repositorios/RepositorioCanciones.java
+++ AppMusic/src/main/java/umu/tds/dominio/repositorios/RepositorioCanciones.java
@@ -13,79 +13,74 @@ import umu.tds.persistencia.FactoriaDAO;
 import umu.tds.persistencia.IAdaptadorCancionDAO;
 
 public class RepositorioCanciones {
-	private Map<String,Cancion> canciones;
+	private Map<String, Cancion> canciones;
 	private static RepositorioCanciones unicaInstancia = new RepositorioCanciones();
-	
+
 	private FactoriaDAO dao;
 	private IAdaptadorCancionDAO adaptadorCanciones;
-	
+
 	private RepositorioCanciones() {
 		try {
 			dao = FactoriaDAO.getInstancia(FactoriaDAO.DAO_TDS);
 			adaptadorCanciones = dao.getCancionDAO();
-			this.canciones = new HashMap<String,Cancion>();
+			this.canciones = new HashMap<String, Cancion>();
 			this.cargarRepositorio();
 		} catch (DAOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public boolean existeCancion(Cancion c) {
 		return canciones.containsKey(c.getUrl());
 	}
-	
+
 	public static RepositorioCanciones getUnicaInstancia() {
 		return unicaInstancia;
 	}
-	
-	public List<Cancion> getAllCanciones(){
+
+	public List<Cancion> getAllCanciones() {
 		ArrayList<Cancion> listaCanciones = new ArrayList<Cancion>();
-		for(Cancion c: this.canciones.values())
+		for (Cancion c : this.canciones.values())
 			listaCanciones.add(c);
 		return listaCanciones;
 	}
-	
+
 	public void addCancion(Cancion c) {
 		canciones.put(c.getUrl(), c);
 	}
-	
+
 	public void removeCancion(Cancion c) {
 		canciones.remove(c.getUrl());
 	}
-	
+
 	public Cancion getCancion(int codigo) {
-		for(Cancion c: canciones.values()) {
-			if(c.getCodigo() == codigo) return c;
+		for (Cancion c : canciones.values()) {
+			if (c.getCodigo() == codigo)
+				return c;
 		}
 		return null;
 	}
-	
+
 	public Cancion getCancion(String url) {
 		return canciones.get(url);
 	}
-	
+
 	private void cargarRepositorio() throws DAOException {
 		List<Cancion> cancionesBD = adaptadorCanciones.recuperarTodasCanciones();
-		for(Cancion c: cancionesBD)
+		for (Cancion c : cancionesBD)
 			this.canciones.put(c.getUrl(), c);
 	}
 
 	public List<Cancion> obtenerTopDiez() {
-		return canciones.values().stream()
-				.sorted(Comparator.comparingInt(Cancion::getNumRep).reversed())
-				.limit(10)
+		return canciones.values().stream().sorted(Comparator.comparingInt(Cancion::getNumRep).reversed()).limit(10)
 				.collect(Collectors.toList());
 	}
+
+	public List<Cancion> getByEstilo(String estilo) {
+
+	}
+
+	public List<Cancion> getByInterprete(String estilo) {
+
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
