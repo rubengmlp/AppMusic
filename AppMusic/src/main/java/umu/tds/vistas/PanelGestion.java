5cb2272f087a4987ab22c84523b3214556497ea6
Ruben <rubengmlp@gmail.com>
2024-06-27 03:21:21 +0200

    Creo panel Gestion PlayLists

--- AppMusic/src/main/java/umu/tds/vistas/PanelGestion.java
+++ AppMusic/src/main/java/umu/tds/vistas/PanelGestion.java
@@ -2,34 +2,36 @@ package umu.tds.vistas;
 
 import javax.swing.JPanel;
 import java.awt.GridBagLayout;
+import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
-import javax.swing.JLabel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JTextField;
-import javax.swing.JComboBox;
-import javax.swing.JCheckBox;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.BoxLayout;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.ImageIcon;
 
-public class PanelBuscar extends JPanel {
+import java.awt.Component;
+
+public class PanelGestion extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private JTable table;
 	private JTextField textField;
 	private JTextField textField_1;
-	private JTable table;
 
 	/**
 	 * Create the panel.
 	 */
-	public PanelBuscar() {
+	public PanelGestion() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{0, 0};
-		gridBagLayout.rowHeights = new int[]{150, 150, 25, 0};
+		gridBagLayout.rowHeights = new int[]{40, 240, 25, 0};
 		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
@@ -42,76 +44,42 @@ public class PanelBuscar extends JPanel {
 		gbc_panel_1.gridy = 0;
 		add(panel_1, gbc_panel_1);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[]{0, 50, 120, 10, 0, 10, 0};
-		gbl_panel_1.rowHeights = new int[]{10, 0, 10, 0, 10, 0, 0};
+		gbl_panel_1.columnWidths = new int[]{20, 50, 120, 10, 0, 10, 0};
+		gbl_panel_1.rowHeights = new int[]{10, 0, 10, 10, 0};
 		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
 		panel_1.setLayout(gbl_panel_1);
 		
 		JLabel lblTitulo = new JLabel("Título");
 		GridBagConstraints gbc_lblTitulo = new GridBagConstraints();
-		gbc_lblTitulo.insets = new Insets(0, 0, 5, 5);
 		gbc_lblTitulo.anchor = GridBagConstraints.EAST;
+		gbc_lblTitulo.insets = new Insets(0, 0, 5, 5);
 		gbc_lblTitulo.gridx = 1;
 		gbc_lblTitulo.gridy = 1;
 		panel_1.add(lblTitulo, gbc_lblTitulo);
 		
-		textField = new JTextField();
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.insets = new Insets(0, 0, 5, 5);
-		gbc_textField.gridx = 2;
-		gbc_textField.gridy = 1;
-		panel_1.add(textField, gbc_textField);
-		textField.setColumns(10);
-		
-		JCheckBox chckbxFav = new JCheckBox("Favoritas");
-		GridBagConstraints gbc_chckbxFav = new GridBagConstraints();
-		gbc_chckbxFav.insets = new Insets(0, 0, 5, 5);
-		gbc_chckbxFav.gridx = 4;
-		gbc_chckbxFav.gridy = 2;
-		panel_1.add(chckbxFav, gbc_chckbxFav);
-		
-		JLabel lblInterpete = new JLabel("Intérpete");
-		GridBagConstraints gbc_lblInterpete = new GridBagConstraints();
-		gbc_lblInterpete.anchor = GridBagConstraints.EAST;
-		gbc_lblInterpete.insets = new Insets(0, 0, 5, 5);
-		gbc_lblInterpete.gridx = 1;
-		gbc_lblInterpete.gridy = 3;
-		panel_1.add(lblInterpete, gbc_lblInterpete);
-		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_1.gridx = 2;
-		gbc_textField_1.gridy = 3;
+		gbc_textField_1.gridy = 1;
 		panel_1.add(textField_1, gbc_textField_1);
 		
-		JButton btnBuscar = new JButton("Buscar");
-		GridBagConstraints gbc_btnBuscar = new GridBagConstraints();
-		gbc_btnBuscar.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnBuscar.insets = new Insets(0, 0, 5, 5);
-		gbc_btnBuscar.gridx = 4;
-		gbc_btnBuscar.gridy = 4;
-		panel_1.add(btnBuscar, gbc_btnBuscar);
+		JPanel panel_3 = new JPanel();
+		GridBagConstraints gbc_panel_3 = new GridBagConstraints();
+		gbc_panel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_panel_3.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel_3.gridx = 2;
+		gbc_panel_3.gridy = 2;
+		panel_1.add(panel_3, gbc_panel_3);
 		
-		JLabel lblEstilo = new JLabel("Estilo");
-		GridBagConstraints gbc_lblEstilo = new GridBagConstraints();
-		gbc_lblEstilo.anchor = GridBagConstraints.EAST;
-		gbc_lblEstilo.insets = new Insets(0, 0, 0, 5);
-		gbc_lblEstilo.gridx = 1;
-		gbc_lblEstilo.gridy = 5;
-		panel_1.add(lblEstilo, gbc_lblEstilo);
+		JButton btnNewButton_6 = new JButton("Crear");
+		panel_3.add(btnNewButton_6);
 		
-		JComboBox comboBox = new JComboBox();
-		GridBagConstraints gbc_comboBox = new GridBagConstraints();
-		gbc_comboBox.insets = new Insets(0, 0, 0, 5);
-		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox.gridx = 2;
-		gbc_comboBox.gridy = 5;
-		panel_1.add(comboBox, gbc_comboBox);
+		JButton btnNewButton_7 = new JButton("Eliminar lista");
+		panel_3.add(btnNewButton_7);
 		
 		JPanel panel_2 = new JPanel();
 		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
@@ -126,6 +94,8 @@ public class PanelBuscar extends JPanel {
 		
 		table = new JTable();
 		scrollPane.setViewportView(table);
+		
+		table = new JTable();
 		
 		JPanel panel = new JPanel();
 		GridBagConstraints gbc_panel = new GridBagConstraints();
@@ -184,7 +154,7 @@ public class PanelBuscar extends JPanel {
 		gbc_btnNewButton_4.gridy = 0;
 		panel.add(btnNewButton_4, gbc_btnNewButton_4);
 		
-		JButton btnNewButton_5 = new JButton("Añadir a lista");
+		JButton btnNewButton_5 = new JButton("Eliminar de la lista");
 		GridBagConstraints gbc_btnNewButton_5 = new GridBagConstraints();
 		gbc_btnNewButton_5.insets = new Insets(0, 0, 0, 5);
 		gbc_btnNewButton_5.gridx = 6;
@@ -192,5 +162,6 @@ public class PanelBuscar extends JPanel {
 		panel.add(btnNewButton_5, gbc_btnNewButton_5);
 
 	}
+
 
 }
